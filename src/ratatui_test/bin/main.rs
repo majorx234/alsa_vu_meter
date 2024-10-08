614033996c1d6887aca26510673b9748343f2aca
MajorX234 <majorx234@googlemail.com>
2024-10-08 23:21:15 +0200

    Move functionality to own function + add event listener

--- src/ratatui_test/bin/main.rs
+++ src/ratatui_test/bin/main.rs
@@ -1,4 +1,7 @@
-use std::{env::args, io::stdout};
+use std::{
+    env::args,
+    io::{stdout, Stdout},
+};
 
 use color_eyre::Result;
 use crossterm::{
@@ -17,41 +20,49 @@ use ratatui::{
 
 fn main() -> Result<()> {
     // TODO retrive real infos from ALSA
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    color_eyre::install()?;
+    let terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    let app_result = run(terminal);
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+fn run(mut terminal: Terminal<CrosstermBackend<Stdout>>) -> Result<()> {
     let titles = ["card0", "card1"];
     let number_of_devices = 2;
     let number_of_channels: usize = 10;
     let channel_values: Vec<f64> = vec![
         0.1, 0.15, 0.2, 0.225, 0.25, 0.275, 0.3, 0.3125, 0.325, 0.3375,
     ];
-    stdout().execute(EnterAlternateScreen)?;
-    enable_raw_mode()?;
-    color_eyre::install()?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-    terminal.draw(|frame| {
-        for i in 0..number_of_devices {
-            let area = Rect::new(0, i as u16, frame.size().width, 1);
-            let bars: Vec<Bar> = channel_values
-                .iter()
-                .enumerate()
-                .map(|(index, value)| {
-                    Bar::default()
-                        .value(((100.0 * *value) as i32).try_into().unwrap())
-                        .label(Line::from(format!("{index}")))
-                })
-                .collect();
-            let title = titles[i];
-            let barchart = BarChart::default()
-                .data(BarGroup::default().bars(&bars))
-                .block(Block::new().title(format!("{title}")))
-                .bar_width(5);
-            frame.render_widget(barchart, area);
+    loop {
+        terminal.draw(|frame| {
+            for i in 0..number_of_devices {
+                let area = Rect::new(0, i as u16, frame.size().width, 1);
+                let bars: Vec<Bar> = channel_values
+                    .iter()
+                    .enumerate()
+                    .map(|(index, value)| {
+                        Bar::default()
+                            .value(((100.0 * *value) as i32).try_into().unwrap())
+                            .label(Line::from(format!("{index}")))
+                    })
+                    .collect();
+                let title = titles[i];
+                let barchart = BarChart::default()
+                    .data(BarGroup::default().bars(&bars))
+                    .block(Block::new().title(format!("{title}")))
+                    .bar_width(5);
+                frame.render_widget(barchart, area);
+            }
+        })?;
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
         }
-    });
-    stdout().execute(LeaveAlternateScreen)?;
-    disable_raw_mode()?;
-    Ok(())
-}
-
-fn run() -> Result<()> {
+    }
     Ok(())
 }
